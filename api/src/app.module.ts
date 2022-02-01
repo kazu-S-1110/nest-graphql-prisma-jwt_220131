ca49509383dc9ce73a5fb5293385c73052067c99
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-02 08:03:16 +0900

    User resourceを作成し、serviceを定義。

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,12 +4,14 @@ import { join } from 'path'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service'
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), "src/schema.gql")
-    })
+    }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
