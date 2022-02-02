9cd523916a69f81ca66ccf433035d604ce7d8d29
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-03 08:24:11 +0900

    graphql用にオーバーライド、authModuleを編集。

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { UsersModule } from 'src/users/users.module'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { LocalStrategy } from './strategies/local.strategy'
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions:{expiresIn:"1h"}
+    })
+  ],
+  providers: [AuthService, AuthResolver, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
