270d121f0821ebd9d386f6d7d871c0b2612f5f16
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-03 12:50:21 +0900

    auth.resolverを設定、動作確認おk。何がどうなっているかわからんことがある。。。

--- api/src/auth/auth.resolver.ts
+++ api/src/auth/auth.resolver.ts
@@ -1,4 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common'
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service'
+import { LoginResponse } from './dto/login-response'
+import { LoginUserInput } from './dto/login-user.input'
+import { GqlAuthGuard } from './guards/gql-auth.guard'
 
 @Resolver()
-export class AuthResolver {}
+export class AuthResolver {
+  constructor(private readonly authService: AuthService) { }
+  
+  @Mutation(()=> LoginResponse)
+  @UseGuards(GqlAuthGuard)
+  async login(
+    @Args("loginUserInput") LoginUserInput: LoginUserInput,
+    @Context() context
+  ) {
+    return this.authService.login(context.user)
+  }
+}
